9512264754b1d2725bd72f1582da9d9f46d47ec2
HelingCode <929926540@qq.com>
2021-08-27 17:57:20 +0800

    增加自定义配置项示例

--- src/main/java/cn/cstube/wiki/controller/TestController.java
+++ src/main/java/cn/cstube/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.cstube.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${spring.application.name}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("hello/post")
