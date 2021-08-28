8b0780051086f2828bc018ab74bcfdbb142bdb16
HelingCode <929926540@qq.com>
2021-08-28 11:21:25 +0800

    制作CopyUtil封装BeanUtils

--- src/main/java/cn/cstube/wiki/service/EbookService.java
+++ src/main/java/cn/cstube/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import cn.cstube.wiki.domain.EbookExample;
 import cn.cstube.wiki.mapper.EbookMapper;
 import cn.cstube.wiki.req.EbookReq;
 import cn.cstube.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import cn.cstube.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,12 +27,13 @@ public class EbookService {
          criteria.andNameLike("%" + req.getName() + "%");
          List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-         List<EbookResp> respList = new ArrayList<>();
-         for (Ebook ebook : ebookList) {
-             EbookResp ebookResp = new EbookResp();
-             BeanUtils.copyProperties(ebook,ebookResp);
-             respList.add(ebookResp);
-         }
-         return respList;
+//         List<EbookResp> respList = new ArrayList<>();
+//         for (Ebook ebook : ebookList) {
+//             //对象复制
+//             respList.add(CopyUtil.copy(ebook,EbookResp.class));
+//         }
+         //列表复制
+         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+         return list;
      }
 }
