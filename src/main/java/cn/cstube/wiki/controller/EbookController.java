3e6cbff77c43491f0413495cda5bb7cf2bf20544
HelingCode <929926540@qq.com>
2021-08-28 10:27:42 +0800

    增加通用的返回类CommonResp,以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/cn/cstube/wiki/controller/EbookController.java
+++ src/main/java/cn/cstube/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package cn.cstube.wiki.controller;
 
 import cn.cstube.wiki.domain.Ebook;
+import cn.cstube.wiki.resp.CommonResp;
 import cn.cstube.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,10 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
