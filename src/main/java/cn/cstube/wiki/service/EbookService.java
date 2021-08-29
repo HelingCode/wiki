81059215507bb64d42bf4448df8db42b46c5d2f1
HelingCode <929926540@qq.com>
2021-08-29 16:35:36 +0800

    集成PageHelper插件，将mapper日志级别改为trace,打印所有的sql日志

--- src/main/java/cn/cstube/wiki/service/EbookService.java
+++ src/main/java/cn/cstube/wiki/service/EbookService.java
@@ -5,7 +5,12 @@ import cn.cstube.wiki.domain.EbookExample;
 import cn.cstube.wiki.mapper.EbookMapper;
 import cn.cstube.wiki.req.EbookReq;
 import cn.cstube.wiki.resp.EbookResp;
+import cn.cstube.wiki.resp.PageResp;
 import cn.cstube.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -19,24 +24,40 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
-     public List<EbookResp> list(EbookReq req){
+     public PageResp<EbookResp> list(EbookReq req){
+
+
          EbookExample ebookExample = new EbookExample();
          EbookExample.Criteria criteria = ebookExample.createCriteria();
           if(!ObjectUtils.isEmpty(req.getName())){
               criteria.andNameLike("%" + req.getName() + "%");
           }
+         PageHelper.startPage(req.getPage(),req.getSize());
          List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+         PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+         LOG.info("总行数：{}", pageInfo.getTotal());
+         LOG.info("总页数：{}", pageInfo.getPages());
+
 
 //         List<EbookResp> respList = new ArrayList<>();
 //         for (Ebook ebook : ebookList) {
 //             //对象复制
 //             respList.add(CopyUtil.copy(ebook,EbookResp.class));
 //         }
+
+
          //列表复制
          List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
-         return list;
+
+         PageResp<EbookResp> pageResp = new PageResp();
+         pageResp.setTotal(pageInfo.getTotal());
+         pageResp.setList(list);
+         return pageResp;
      }
 }
