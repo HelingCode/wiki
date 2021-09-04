4f0cd953977730fe1c044af52f0836acb0ff0850
HelingCode <929926540@qq.com>
2021-09-04 09:14:24 +0800

    从分类管理复制出文档管理的前后端代码 遗留问题：编辑表单中，只能选择一级文档作为父文档

---
+++ src/main/java/cn/cstube/wiki/controller/DocController.java
@@ -0,0 +1,52 @@
+package cn.cstube.wiki.controller;
+
+import cn.cstube.wiki.req.DocQueryReq;
+import cn.cstube.wiki.req.DocSaveReq;
+import cn.cstube.wiki.resp.DocQueryResp;
+import cn.cstube.wiki.resp.CommonResp;
+import cn.cstube.wiki.resp.PageResp;
+import cn.cstube.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+@CrossOrigin
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req) {
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+}
---
+++ src/main/java/cn/cstube/wiki/req/DocQueryReq.java
@@ -0,0 +1,8 @@
+package cn.cstube.wiki.req;
+
+public class DocQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
